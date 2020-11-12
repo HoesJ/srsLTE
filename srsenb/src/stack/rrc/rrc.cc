ff0c891942e54a3aa8ccd6e30fe4c47cea2b5c09
Jochem Hoes <jochem.hoes@non.keysight.com>
2020-11-12 14:45:45 +0100

    attempt

--- srsenb/src/stack/rrc/rrc.cc
+++ srsenb/src/stack/rrc/rrc.cc
@@ -2294,16 +2294,12 @@ void rrc::ue::send_dl_ccch(dl_ccch_msg_s* dl_ccch_msg)
 
     // ###CHANGE###: introduce error in byte buffer
     parent->rrc_log->debug("###CHANGE###");
-    parent->rrc_log->debug("before altering");
 
-    parent->rrc_log->debug_hex(pdu->msg, pdu->N_bytes);
-    parent->rrc_log->debug_hex(pdu->buffer, pdu->N_bytes);
-
-    parent->rrc_log->debug("After altering");
+    parent->rrc_log->debug_hex(pdu->msg, pdu->N_bytes, "before altering");
+    parent->rrc_log->debug_hex(pdu->buffer, pdu->N_bytes, " ");
     pdu->buffer[5] = 0xff;
-    
-    parent->rrc_log->debug_hex(pdu->msg, pdu->N_bytes);
-    parent->rrc_log->debug_hex(pdu->buffer, pdu->N_bytes);
+    parent->rrc_log->debug_hex(pdu->msg, pdu->N_bytes, "After altering");
+    parent->rrc_log->debug_hex(pdu->buffer, pdu->N_bytes, " ");
 
     parent->rrc_log->debug("###END CHANGE###");
     //
